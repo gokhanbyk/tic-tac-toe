925d30b8fd1e23cdf2b4b3cc0391441da581c6bb
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-01-28 14:52:59 +0300

    computed values

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,26 @@ import { useState } from "react"
 
 import GameBoard from "./components/GameBoard"
 import Player from "./components/Player"
+import Log from "./components/Log"
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([])
+  // lifting state up
   const [activePlayer, setActivePlayer] = useState('X')
 
-  function handleSelectSquare() {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X')
 
+  function handleSelectSquare(rowIndex, colIndex) {
+    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X')
+    setGameTurns(prevTurns => {
+      let currentPlayer = 'X'
+
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O'
+      }
+
+      const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevTurns]
+      return updatedTurns
+    })
   }
 
   return (
@@ -22,7 +35,7 @@ function App() {
         <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
       </div>
 
-      LOG
+      <Log />
     </main>
   )
 }
