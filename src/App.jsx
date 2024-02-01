2927d3b8cdb56072c00edd4e8c2200e73904826f
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-02-01 20:35:39 +0300

    updating players data

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,10 @@ function deriveActivePlayer(gameTurns) {
 
 
 function App() {
+  const [players, setPlayers] = useState({
+    'X': 'Player 1',
+    'O': 'Player 2',
+  })
   const [gameTurns, setGameTurns] = useState([])
   // const [hasWinner, setHasWinner] = useState(false)
   // lifting state up
@@ -66,6 +70,15 @@ function App() {
 
   function handleRestart() {
     setGameTurns([])
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName
+      }
+    })
   }
 
   return (
