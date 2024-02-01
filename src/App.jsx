7698c733e48e5e60926e9eb31d86ea821abb7a61
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-02-01 20:28:03 +0300

    handleRestart func

--- src/App.jsx
+++ src/App.jsx
@@ -64,6 +64,10 @@ function App() {
     })
   }
 
+  function handleRestart() {
+    setGameTurns([])
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -71,7 +75,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner} />}
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
 
